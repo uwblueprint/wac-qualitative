1c32c902222764b34bff6e0cdf4f8c07f2c268b7
Jason Fok <jyffok@uwaterloo.ca>
2019-03-23 15:57:13 -0400

    Add initial store and fix format

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { Provider } from 'react-redux'; // redux bindings for react
 import thunk from 'redux-thunk'; // to use promises for asynchronous actions
 import { createStore, applyMiddleware, compose } from 'redux'; // to create the store and middleware
+
+import './index.css';
+import App from './App';
 import reducers from './reducers/index.js';
 import * as serviceWorker from './serviceWorker';
+import initialStore from './initialStore';
 
 const middleware = [thunk];
-const store = createStore(reducers, {}, compose(applyMiddleware(...middleware)));
+const store = createStore(
+    reducers,
+    initialStore,
+    compose(applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
