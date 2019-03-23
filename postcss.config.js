c516568174032e04dba27a24a3ed01043649dd36
Stephen Yang <sy@stephenyang.net>
2019-03-23 15:41:10 -0400

    Address PR Change

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
-const postcssPresetEnv = require('postcss-preset-env');
-
 module.exports = {
   plugins: [
     require("autoprefixer")(),
     require("postcss-import"),
     require('postcss-advanced-variables'),
-    postcssPresetEnv(),
+    require('postcss-preset-env')(),
     require("postcss-extend"),
     require("postcss-mixins")
   ]
