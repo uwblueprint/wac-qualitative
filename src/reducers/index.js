0b313fbfd94b3077ab896bf6840263bb23122abd
Jason Fok <jyffok@uwaterloo.ca>
2019-03-30 16:45:27 -0400

    Implement redux-storage to load old state on refresh

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import * as storage from 'redux-storage'
+
 import { count } from '../reducers/countReducer';
 
-const reducers = combineReducers({
+const reducers = storage.reducer(combineReducers({
   count: count
-});
+}));
 
 export default reducers;
