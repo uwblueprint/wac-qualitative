891f3293bfb941fbc372e499f00e8ac62c7cadda
Dendrimer <franzjulius@gmail.com>
2019-04-01 19:57:48 -0400

    Separate components outside of App

--- src/App.js
+++ src/App.js
@@ -2,34 +2,21 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
 } from 'react-router-dom';
+import Intro from './components/Intro.jsx';
+import Questionaire from './components/Questionaire.jsx';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Link to={{ pathname: `/questionaire/1` }}>Questionaire</Link>
-        </div>
-        <Route exact path={`/questionaire/:pageNum`} component={Questionaire} />
+        <Route exact path={`/`} component={Intro} />
+        <Route exact path={`/questionaire/:pageNum`} render={({ match }) =>
+          <Questionaire pageNum={parseInt(match.params.pageNum)} />} />
       </Router>
     );
   }
-}
-
-// TODO: Replace this with the actual questionaire component
-const Questionaire = ({ match }) => {
-  const pageNum = parseInt(match.params.pageNum)
-  return (
-    <div>
-      <h1>Question {parseInt(pageNum)}</h1>
-      <div>
-        <Link to={{ pathname: `/questionaire/${(pageNum + 1).toString()}` }}>Questionaire Page {(pageNum + 1).toString()}</Link>
-      </div>
-    </div >
-  )
 }
 
 export default App;
