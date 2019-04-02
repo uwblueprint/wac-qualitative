b39548f3aa4a2ca2ab5b56b43e247256f08c2495
Sophie <sophieeqin@gmail.com>
2019-04-01 20:47:43 -0400

    changed margins

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,10 +5,16 @@ import './styles/header.css';
 class Header extends Component {
     render() {
         return <div className="header-container">
-            <h2 className="text header">Dataset Bias Assessment Tool</h2>
+            <div className="container">
+                <div>
+                    <h2 className="text header">Dataset Bias Assessment Tool</h2>
+                </div>
+                <div>
+                    <img className="logo" src={logo} alt="" />
+                </div>
+            </div>
             <p className="text subheader">Section 1/8</p>
             <h1 className="text title">Progress Title</h1>
-            <img className="logo" src={logo} alt="" />
         </div>
 
     }
