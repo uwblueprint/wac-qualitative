9f1e23bd33b439473b010ce7d650d59e962f9757
wrjren <winnierjren@gmail.com>
2019-04-01 21:10:08 -0400

    title page card done

--- src/TitlePage.jsx
+++ src/TitlePage.jsx
@@ -7,45 +7,30 @@ import './TitlePage.css';
 class TitlePage extends Component {
 
   render() {
-    var cardStyle ={
-      color: "black",
-      background: "#FFFFFF",
-  /* CARD STROKE STYLE */
-  border: "1px solid #E1E1E1",
-  width: '300px',
-  boxSizing: "border-box",/* Card drop shadow */
-  boxShadow: '1px 1px 8px rgba(0, 0, 0, 0.07)',
-  borderRadius: '2px',
-  position: 'absolute',
-width:' 840px',
-height: '668px',
-left: '300px',
-top: '68px',
-textalign:'left'
-    }; 
 
     return (
       <div>
       <div className="cardStyle">
-      <p>hello world!!!!!!!</p>
-      <div >
+      <div className="introText">
+      
 <img  src={logo} alt="Logo"/>
-<img  src={avatar} alt="Avatar"/>
-      <p>Welcome to the We All Count Data Biography Builder.</p>
+      <h3>Welcome to the We All Count Data Biography&nbsp;Builder.</h3>
+    <p>This tool is designed around the Data Equity Biography checklist. The tools helps you identify potential sources of bias in your dataset and gives you the chance to improve the equity in your use of the data. </p>
+    <p>This tool is designed to be used to assess the level of bias that is present in dataset. This is designed to assess the level of bias prior to analysis. To get a sense of the sources and locations of potential bias in your data most completely you’ll need the a copy of the survey tool that was used to collect the data, the codebook, and the document that tells you how the data was sampled and collected.</p>
+
+<div className="buttonStyle" >
+<p>Begin</p>
       </div>
-      <p>This tool is designed around the Data Equity Biography checklist. The tools helps you identify potential sources of bias in your dataset and gives you the chance to improve the equity in your use of the data.
 
-This tool is designed to be used to assess the level of bias that is present in dataset. This is designed to assess the level of bias prior to analysis. To get a sense of the sources and locations of potential bias in your data most completely you’ll need the a copy of the survey tool that was used to collect the data, the codebook, and the document that tells you how the data was sampled and collected.
-
-</p>
-      <div className="buttonStyle" >
-      Begin
+<div className="introPic">
+<img  src={avatar} alt="Avatar"/>
+</div>
       </div>
       </div>
       <div className="bottomBorder"> 
       <div className ="bottomTextL">
       <h3>Who we are</h3>
-      <p>We All Count is a project dedicated to equity in data science. Read all about the project here.</p>
+      <p>We All Count is a project dedicated to equity in data science. Read all about the project <a>here</a>.</p>
       </div>
       <div className ="bottomTextM">
       <h3>How Does It Work?</h3>
@@ -54,9 +39,12 @@ This tool is designed to be used to assess the level of bias that is present in 
       </div>
       <div className ="bottomTextR">
       <h3>Speak To A Specialist</h3>
-      <p>Would you like one of our specialists to get back to you with some expert advice on the potential bias in your dataset? If so, please click here.</p>
+      <p>Would you like one of our specialists to get back to you with some expert advice on the potential bias in your dataset? If so, please click <a>here</a>.</p>
       </div>
       
+      </div>
+      <div className="secondBottomBorder">
+      <p>Copyright @ WeAllCount 2019</p>
       </div>
       </div>
     );
