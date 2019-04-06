9fdba9e37351cc97b971f482b46f477dd83d6319
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2019-04-06 16:56:31 -0400

    add pageIndicator into the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
-
+import logo from "../logo.png";
 import "../styles/header.css";
 import PageIndicatorContainer from "../containers/PageIndicatorContainer";
 
 class Header extends Component {
   render() {
-    const currentPage = this.props.pageNum;
-    const totalPages = "8";
+    var currentPage = "1";
+    var totalPages = "8";
     return (
       <div className="header-container">
         <div>
-          <img className="logo" src="/logo.png" alt="" />
+          <img className="logo" src={logo} alt="" />
         </div>
         <h2 className="text header">Dataset Bias Assessment Tool</h2>
         <p className="text subheader">
@@ -18,7 +18,7 @@ class Header extends Component {
         </p>
         <h1 className="text title">Progress Title</h1>
         <div className="indicator-container">
-          <PageIndicatorContainer pageNum={currentPage} />
+          <PageIndicatorContainer />
         </div>
       </div>
     );
