861cd09236129d538cd52dac393d76c0de96ef08
wrjren <winnierjren@gmail.com>
2019-04-06 16:25:11 -0400

    changed it to a dumb component

--- src/TitlePage.jsx
+++ src/TitlePage.jsx
@@ -4,70 +4,65 @@ import avatar from "./styles/images/personIcon.png";
 import "./App.css";
 import "./TitlePage.css";
 
-class TitlePage extends Component {
-  render() {
-    return (
-      <div>
-        <div className="cardStyle">
-          <div className="introText">
-            <img style={{ marginBottom: "5%" }} src={logo} alt="Logo" />
-            <h3>Welcome to the We All Count Data Biography&nbsp;Builder.</h3>
-            <p>
-              This tool is designed around the Data Equity Biography checklist.
-              The tools helps you identify potential sources of bias in your
-              dataset and gives you the chance to improve the equity in your use
-              of the data.{" "}
-            </p>
-            <p>
-              This tool is designed to be used to assess the level of bias that
-              is present in dataset. This is designed to assess the level of
-              bias prior to analysis. To get a sense of the sources and
-              locations of potential bias in your data most completely you’ll
-              need the a copy of the survey tool that was used to collect the
-              data, the codebook, and the document that tells you how the data
-              was sampled and collected.
-            </p>
+const TitlePage = props => {
+  return (
+    <div>
+      <div className="cardStyle">
+        <div className="introText">
+          <img style={{ marginBottom: "5%" }} src={logo} alt="Logo" />
+          <h3>Welcome to the We All Count Data Biography&nbsp;Builder.</h3>
+          <p>
+            This tool is designed around the Data Equity Biography checklist.
+            The tools helps you identify potential sources of bias in your
+            dataset and gives you the chance to improve the equity in your use
+            of the data.{" "}
+          </p>
+          <p>
+            This tool is designed to be used to assess the level of bias that is
+            present in dataset. This is designed to assess the level of bias
+            prior to analysis. To get a sense of the sources and locations of
+            potential bias in your data most completely you’ll need the a copy
+            of the survey tool that was used to collect the data, the codebook,
+            and the document that tells you how the data was sampled and
+            collected.
+          </p>
 
-            <div className="buttonStyle">
-              <p>Begin</p>
-            </div>
+          <div className="buttonStyle">
+            <p>Begin</p>
+          </div>
 
-            <div className="introPic">
-              <img src={avatar} alt="Avatar" />
-            </div>
+          <div className="introPic">
+            <img src={avatar} alt="Avatar" />
           </div>
-        </div>
-        <div className="bottomBorder">
-          <div className="bottomTextL">
-            <h3>Who we are</h3>
-            <p>
-              We All Count is a project dedicated to equity in data science.
-              Read all about the project{" "}
-              <a href="https://weallcount.com/">here</a>.
-            </p>
-          </div>
-          <div className="bottomTextM">
-            <h3>How Does It Work?</h3>
-            <p>
-              Want to learn more about the process? Find out using the link
-              below.
-            </p>
-          </div>
-          <div className="bottomTextR">
-            <h3>Speak To A Specialist</h3>
-            <p>
-              Would you like one of our specialists to get back to you with some
-              expert advice on the potential bias in your dataset? If so, please
-              click <a href="mailto:heather@idatassist.com">here</a>.
-            </p>
-          </div>
-        </div>
-        <div className="secondBottomBorder">
-          <i>Copyright @ WeAllCount 2019</i>
         </div>
       </div>
-    );
-  }
-}
-
+      <div className="bottomBorder">
+        <div className="bottomTextL">
+          <h3>Who we are</h3>
+          <p>
+            We All Count is a project dedicated to equity in data science. Read
+            all about the project <a href="https://weallcount.com/">here</a>.
+          </p>
+        </div>
+        <div className="bottomTextM">
+          <h3>How Does It Work?</h3>
+          <p>
+            Want to learn more about the process? Find out using the link below.
+          </p>
+        </div>
+        <div className="bottomTextR">
+          <h3>Speak To A Specialist</h3>
+          <p>
+            Would you like one of our specialists to get back to you with some
+            expert advice on the potential bias in your dataset? If so, please
+            click <a href="mailto:heather@idatassist.com">here</a>.
+          </p>
+        </div>
+      </div>
+      <div className="secondBottomBorder">
+        <i>Copyright @ WeAllCount 2019</i>
+      </div>
+    </div>
+  );
+};
 export default TitlePage;
