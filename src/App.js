ee573f5c5fa7a9db005376aee54cf60cf446bb2d
Winnie <39350447+wrjren@users.noreply.github.com>
2019-04-06 18:28:49 -0400

    Title page (#27)
    * Title Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Intro from "./components/Intro.jsx";
 import Questionaire from "./components/Questionaire.jsx";
+import TitlePage from './components/TitlePage';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route exact path={`/`} component={Intro} />
-        <Route
-          exact
-          path={`/questionaire/:pageNum`}
-          render={({match: {params: { pageNum }}}) => 
-            <Questionaire pageNum={parseInt(pageNum)} />}
-        />
+        <Route exact path={`/`} component={TitlePage} />
+        <Route exact path={`/questionaire/:pageNum`} render={({ match: { params: { pageNum } } }) =>
+          <Questionaire pageNum={parseInt(pageNum)} />} />
       </Router>
     );
   }
