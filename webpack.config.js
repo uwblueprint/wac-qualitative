b40ccdd857f2b451d79b754f0e0733877d2ca7a8
Austin Jiang <austin.jiang@uwaterloo.ca>
2019-06-06 20:00:37 -0400

    fix merge conflict

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ module.exports = {
     compress: true,
     port: 3000,
     // webpack routing fix to add new routes in App.js
-    // see hhttps://bit.ly/2wwnRxZ for a further explanation
+    // see https://bit.ly/2wwnRxZ for a further explanation
     publicPath: '/',
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   entry: path.resolve(__dirname, "./src/index.js"),
   module: {
@@ -44,15 +44,10 @@ module.exports = {
     ]
   },
   output: {
-    path: __dirname + "/dist",
+    path: __dirname + '/dist',
     filename: "bundle.js",
     chunkFilename: "[name].js"
   },
-  // output: {
-  //   path: path.resolve(__dirname, 'dist'),
-  //   filename: 'js/[name].min.js',
-  //   publicPath: '/'
-  // },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "style.css"
