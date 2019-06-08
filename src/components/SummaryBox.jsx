c515b206bbdb78c72bd544f432b5c70a5e4273e2
Austin Jiang <austin.jiang@uwaterloo.ca>
2019-06-07 23:39:34 -0400

    Add ternary operators to reduce code repetition

--- src/components/SummaryBox.jsx
+++ src/components/SummaryBox.jsx
@@ -13,93 +13,37 @@ class SummaryBox extends Component {
     const val = this.props.val;
 
     // YES
-    if (val == 0) {
-      return (
-        <div className="figmaCard">
-          <div className="boxboxbox">
-            <h1 className="sectionNum"> Section {sectionNumber} </h1>
-            <h1 className="sectionTitle"> {sectionTitle} </h1>
-          </div>
-          <div id="bar" />
-          <div className="outer-circle red-b" id="left-circle">
-            <div className="inner-circle">
-              <p className="circle-text red-c" id="circle-left-text">
-                Biased
-              </p>
-            </div>
-          </div>
-          <div className="outer-circle">
-            <div className="inner-circle">
-              <p className="circle-text" id="circle-center-text">
-                Somewhat Biased
-              </p>
-            </div>
-          </div>
-          <div className="outer-circle">
-            <div className="inner-circle">
-              <p className="circle-text" id="circle-right-text">
-                Not Biased
-              </p>
-            </div>
-          </div>
-          <div id="rectangle" className="red-b" />
-        </div>
-      );
-    } else if (val == 1) {
-      // MAYBE
-      return (
+    return (
       <div className="figmaCard">
         <div className="boxboxbox">
           <h1 className="sectionNum"> Section {sectionNumber} </h1>
           <h1 className="sectionTitle"> {sectionTitle} </h1>
         </div>
-        <div id="bar"></div>
-        <div className="outer-circle" id="left-circle">
+        <div id="bar" />
+        <div className={`outer-circle ${val == 0 ? "red-b" : ""}`} id="left-circle">
           <div className="inner-circle">
-            <p className="circle-text" id="circle-left-text">Biased</p>
+            <p className={`circle-text ${val == 0 ? "red-c" : ""}`} id='circle-left-text'>
+              Biased
+            </p>
           </div>
         </div>
-        <div className="outer-circle yellow-b">
+        <div className={`outer-circle ${val == 1 ? "yellow-b" : ""}`} id='left-circle'>
           <div className="inner-circle">
-            <p className="circle-text yellow-c" id="circle-center-text">Somewhat Biased</p>
+            <p className={`circle-text ${val == 1 ? "yellow-c" : ""}`} id='circle-center-text'>
+              Somewhat Biased
+            </p>
           </div>
         </div>
-        <div className="outer-circle">
+        <div className={`outer-circle ${val == 2 ? "green-b" : ""}`} id='left-circle'>
           <div className="inner-circle">
-            <p className="circle-text" id="circle-right-text">Not Biased</p>
+            <p className={`circle-text ${val == 2 ? "green-c" : ""}`} id="circle-right-text">
+              Not Biased
+            </p>
           </div>
         </div>
-        <div id="rectangle" className="yellow-b"></div>
+        <div id="rectangle" className={`${val == 0 ? 'red-b' : `${val == 1 ? 'yellow-b' : 'green-b'}`}`} />
       </div>
     );
-    } else {
-      // NO
-      return (
-      <div className="figmaCard">
-        <div className="boxboxbox">
-          <h1 className="sectionNum"> Section {sectionNumber} </h1>
-          <h1 className="sectionTitle"> {sectionTitle} </h1>
-        </div>
-        <div id="bar"></div>
-        <div className="outer-circle" id="left-circle">
-          <div className="inner-circle">
-            <p className="circle-text" id="circle-left-text">Biased</p>
-          </div>
-        </div>
-        <div className="outer-circle">
-          <div className="inner-circle">
-            <p className="circle-text" id="circle-center-text">Somewhat Biased</p>
-          </div>
-        </div>
-        <div className="outer-circle green-b">
-          <div className="inner-circle">
-            <p className="circle-text green-c" id="circle-right-text">Not Biased</p>
-          </div>
-        </div>
-        <div id="rectangle" className="green-b"></div>
-      </div>
-    );
-    }
   }
 }
 
