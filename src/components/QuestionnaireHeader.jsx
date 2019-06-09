0e65bc110439a65dfdaba0cdbf36ed1c7458a82f
suzyng83209 <suzyn.one@gmail.com>
2019-06-09 19:32:18 -0400

    tabs instead of spaces

--- src/components/QuestionnaireHeader.jsx
+++ src/components/QuestionnaireHeader.jsx
@@ -7,23 +7,23 @@ import logo from '../../public/images/logo.png';
 import '../styles/header.css';
 
 const QuestionnaireHeader = ({ pageNum }) => (
-    <div className="header-container">
-        <div>
-            <img className="logo" src={`/${logo}`} alt="Logo" />
-        </div>
-        <h2 className="text header">Dataset Bias Assessment Tool</h2>
-        <p className="text subheader">
-            Section {pageNum + 1}/{sections.length}
-        </p>
-        <h1 className="text title">{sections[pageNum].section}</h1>
-        <div className="indicator-container">
-            <ProgressIndicator pageNum={pageNum} sections={sections} />
-        </div>
-    </div>
+	<div className="header-container">
+		<div>
+			<img className="logo" src={`/${logo}`} alt="Logo" />
+		</div>
+		<h2 className="text header">Dataset Bias Assessment Tool</h2>
+		<p className="text subheader">
+			Section {pageNum + 1}/{sections.length}
+		</p>
+		<h1 className="text title">{sections[pageNum].section}</h1>
+		<div className="indicator-container">
+			<ProgressIndicator pageNum={pageNum} sections={sections} />
+		</div>
+	</div>
 );
 
 export default QuestionnaireHeader;
 
 QuestionnaireHeader.propTypes = {
-  pageNum: PropTypes.number,
+	pageNum: PropTypes.number,
 };
