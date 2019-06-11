826d9e38913724eeb47dba170ba6815c07e0e926
loic <muruloic@gmail.com>
2019-06-10 23:38:42 -0400

    Implement csv export

--- src/actions/countActions.js
+++ src/actions/countActions.js
@@ -7,3 +7,38 @@ export const increment = () => ({
 export const decrement = () => ({
   type: types.DECREMENT_COUNT
 });
+
+export const exportCsv = (items) => {
+  let convertJSONToCSV = (objArray) => {
+    // CSVs require carriage-return characters with newlines because of some excel quirks
+    var str = 'Questions,Answers\r\n';
+    for (var i = 0; i < objArray.length; i++) {
+        var line = '';
+        line += objArray[i]["question"] + ',' + objArray[i]["answer"];
+        str += line + '\r\n';
+    }
+    return str;
+  };
+
+  // Convert input to CSV
+  var csv = convertJSONToCSV(items);
+  var fileName = 'wac-survey-results.csv';
+
+  var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+  if (navigator.msSaveBlob) { // IE 10+
+      navigator.msSaveBlob(blob, fileName);
+
+  // Slightly roundabout way of triggering the download
+  } else {
+    var link = document.createElement("a");
+    if (link.download !== undefined) {
+        var url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute("download", fileName);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+  }   
+}
--- src/components/Questionaire.jsx
+++ src/components/Questionaire.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import * as count from "../actions/countActions";
 import Header from "./Header";
 
 // TODO: Replace this with the actual questionaire component
@@ -9,7 +9,15 @@ class Questionaire extends Component {
     return (
       <div>
         <Header pageNum={this.props.pageNum} />
-        <h1>Question {this.props.pageNum.toString()}</h1>
+        {/* Call the export function with test data on click as proof of concept*/}
+        <Link>
+          <span onClick={() => count.exportCsv([{"question":"q1","answer":"a1"}])}>
+            EXPORT CSV
+          </span>
+        </Link>
+        <h1>
+            Question {this.props.pageNum.toString()}
+        </h1>
         <Link
           to={{
             pathname: `/questionaire/${(this.props.pageNum + 1).toString()}`
