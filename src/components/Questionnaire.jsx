7ebc9f202a72a80e83c120e8cc4ac76f9b5c9c1b
loic <muruloic@gmail.com>
2019-06-10 23:59:08 -0400

    Fix merge conflicts

--- src/components/Questionnaire.jsx
+++ src/components/Questionnaire.jsx
@@ -6,13 +6,12 @@ import QuestionnaireHeaderContainer from '../containers/QuestionnaireHeaderConta
 class Questionnaire extends Component {
 	render() {
 		return (
-			
 			<div>
+				<QuestionnaireHeaderContainer />
 				{/* Call the export function with test data on click as proof of concept*/}
 				<span onClick={() => index.exportCsv([{"question":"q1","answer":"a1"}])}>
 					EXPORT CSV
 				</span>
-				<QuestionnaireHeaderContainer />
 				<h1>Section {this.props.pageNum}</h1>
 				Questionnaire Page
 			</div>
