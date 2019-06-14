c54f8fe1680017839e58bd0abe2bd65a0e4626b0
suzyng83209 <suzyn.one@gmail.com>
2019-06-13 20:26:35 -0400

    import correct component

--- src/containers/QuestionnaireSectionContainer.jsx
+++ src/containers/QuestionnaireSectionContainer.jsx
@@ -1,24 +1,24 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { incrementPageNum, decrementPageNum } from '../actions';
-import QuestionnaireHeader from '../components/QuestionnaireHeader';
+import QuestionnaireSection from '../components/QuestionnaireSection';
 
 const mapStateToProps = state => {
-    return {
-        pageNum: state.pageNum,
-    };
+	return {
+		pageNum: state.pageNum,
+	};
 };
 
 const mapDispatchToProps = dispatch =>
-    bindActionCreators(
-        {
-            incrementPageNum,
-            decrementPageNum,
-        },
-        dispatch,
-    );
+	bindActionCreators(
+		{
+			incrementPageNum,
+			decrementPageNum,
+		},
+		dispatch,
+	);
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(QuestionnaireHeader);
+	mapStateToProps,
+	mapDispatchToProps,
+)(QuestionnaireSection);
