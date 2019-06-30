4be1d5ca336ed5ad71310175678b047388af4a28
Austin Jiang <austin.jiang@uwaterloo.ca>
2019-06-30 17:49:15 -0400

    added google analytics support (#51)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import ReactGA from 'react-ga';
+
 
 import Questionnaire from "./components/Questionnaire.jsx";
 import TitlePageContainer from "./containers/TitlePageContainer";
 import SummaryPageContainer from "./containers/SummaryPageContainer";
 
+function initializeReactGA() {
+  ReactGA.initialize('UA-142479675-1');
+  ReactGA.pageview('/');
+}
 
 class App extends Component {
   render() {
