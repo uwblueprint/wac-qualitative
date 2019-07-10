f36c4c1c7dcc81a08de744807da4e25ba29fb865
Rohil Gupta <28664595+rohilG@users.noreply.github.com>
2019-07-10 14:20:19 -0400

    deploying to gh-pages (#55)
    * deploying to gh-pages
    
    * circle shouldn't treat warnings as errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import ReactGA from 'react-ga';
-
+import { HashRouter as Router, Route } from "react-router-dom";
+import ReactGA from "react-ga";
 
 import Questionnaire from "./components/Questionnaire.jsx";
 import TitlePageContainer from "./containers/TitlePageContainer";
 import SummaryPageContainer from "./containers/SummaryPageContainer";
 
 function initializeReactGA() {
-  ReactGA.initialize('UA-142479675-1');
-  ReactGA.pageview('/');
+  ReactGA.initialize("UA-142479675-1");
+  ReactGA.pageview("/");
 }
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Route exact path={`/`} component={TitlePageContainer} />
-        <Route exact path={`/questionnaire`} component={Questionnaire} />
-        <Route exact path={'/summary'} component={SummaryPageContainer}/>
+      <Router basename="/">
+        <Route exact path={"/"} component={TitlePageContainer} />
+        <Route exact path={"/questionnaire"} component={Questionnaire} />
+        <Route exact path={"/summary"} component={SummaryPageContainer} />
       </Router>
     );
   }
